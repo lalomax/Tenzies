0401475460e778fb4bcafb18fc3c80970adfcf83
lalomax <lalomax365@gmail.com>
2023-02-01 17:12:21 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,77 @@
+import { useState, useEffect } from "react";
+import Die from "./components/Die";
+import { nanoid } from "nanoid";
+import Confetti from "react-confetti";
+function App() {
+  const [dice, setDice] = useState(allNewDice());
+  const [tenzies, setTenzies] = useState(false);
+
+  useEffect(() => {
+    const allHeld = dice.every((die) => die.isHeld);
+    const firstValue = dice[0].value;
+    const allSameValue = dice.every((die) => die.value === firstValue);
+    if (allHeld && allSameValue) {
+      setTenzies(true);
+    }
+  }, [dice]);
+
+  function generateNewDie() {
+    return {
+      value: Math.ceil(Math.random() * 6),
+      isHeld: false,
+      id: nanoid(),
+    };
+  }
+  function allNewDice() {
+    return Array.apply(null, { length: 10 }).map(() => {
+      return generateNewDie();
+    });
+  }
+
+  const holdDice = (id) => {
+    setDice((oldDice) =>
+      oldDice.map((die) => {
+        return die.id === id ? { ...die, isHeld: !die.isHeld } : die;
+      })
+    );
+  };
+
+  const diceElements = dice.map((die) => (
+    <Die
+      holdDice={() => holdDice(die.id)}
+      key={die.id}
+      value={die.value}
+      isHeld={die.isHeld}
+    />
+  ));
+
+  const handleRoll = () => {
+    if (!tenzies) {
+      setDice((oldDice) =>
+        oldDice.map((die) => {
+          return die.isHeld ? die : generateNewDie();
+        })
+      );
+    } else {
+      setTenzies(false)
+      setDice(allNewDice())
+    }
+  };
+
+  return (
+    <main>
+      {tenzies && <Confetti />}
+      <h1 className="title">Tenzies</h1>
+      <p className="instructions">
+        Roll until all dice are the same. Click each die to freeze it at its
+        current value between rolls.
+      </p>
+      <div className="dice-container">{diceElements}</div>
+      <button className="roll-dice" onClick={handleRoll}>
+        {tenzies ? "New Game" : "Roll"}
+      </button>
+    </main>
+  );
+}
+
+export default App;
